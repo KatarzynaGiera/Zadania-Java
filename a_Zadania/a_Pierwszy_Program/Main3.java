4fcf0bc82da2acea6f3ef7c45615bbf4fbab2790
Katarzyna Giera <giera.katarzyna@gmail.com>
2019-10-03 22:15:52 +0200

    Rozwiązanie zadania pierwszego

---
+++ a_Zadania/a_Pierwszy_Program/Main3.java
@@ -0,0 +1,15 @@
+/** Komentarz dokumentacyjny - na jego podstawie można wygenerować dokumentację techniczną*/
+public class Main3 {
+
+	public static void main(String[] args) {
+		System.out.println("Pierwszy program JAVA");
+//Komentarz jednolinijkowy
+	}
+/* komentarz
+obejmujący 
+kilka
+linii
+pomiędzy 
+znacznikami*/
+
+}
