2c7ae1a7e9473083dc24b82da8f7472b09f6abf1
Katarzyna Giera <giera.katarzyna@gmail.com>
2019-10-03 22:58:43 +0200

    Rozwiązania zadania drugiego

---
+++ a_Zadania/b_Zmienne_i_typy_danych/Main1.java
@@ -0,0 +1,23 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		//ta zmienna przechowuje typ int
+		int int1 = 21;
+		System.out.println(int1);
+		//ta zmienna przechowuje typ char
+		char char2 = 'c';
+		System.out.println(char2);
+		//ta zmienna przechowuje typ long
+		long long3 = 2112;
+		System.out.println(long3);
+		//ta zmienna przechowyje typ double
+		double double4 =21.12;
+		System.out.println(double4);
+		//ta zmienna przechowuje typ boolean
+		boolean bolean5 = true;
+		System.out.println(bolean5);
+		
+	}
+
+}
---
+++ a_Zadania/b_Zmienne_i_typy_danych/Main3.java
@@ -0,0 +1,14 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		int nr1 = 5;
+		double nr2 = 7.4;
+		//Poniższa zmienna powinna być typu być float, ponieważ przechowywać sumę liczby całkowitej i zmiennoprzecinkowej, która też będzie zmiennoprzecinkowa
+		double result = 0;
+		result = nr1 + nr2;
+		System.out.println(result);
+		
+	}
+
+}
